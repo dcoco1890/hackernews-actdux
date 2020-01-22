0348e6acc113cbbe00441f516143998c2601e32c
Cohen <dcoco91@gmail.com>
2020-01-22 14:42:35 -0500

    added a full height nav that holds queries on the right hand side. was able to add to it via testing with the windowsavequery thing from earlier.

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Nav from "./Nav";
 
 const Main = () => (
-  <div>
-    <nav></nav>
-    <section></section>
+  <div className="row">
+    <div className="col s12 m4 l3">
+      <Nav />
+    </div>
+    <div className="col s12 m8 l9">
+      <section></section>
+    </div>
   </div>
 );
 
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -1,1 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+
+const styles = {
+  query: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#fafafa",
+    color: "green"
+  },
+  nav: {
+    height: "100vh"
+  }
+};
+
+const mapStateToProps = state => {
+  return { queries: state.queries };
+};
+// connected nav component takes in queries (for now, might split later)
+const ConnectedNav = ({ queries }) => (
+  <nav style={styles.nav}>
+    <div className="search">
+      <input type="text" />
+    </div>
+    <div className="queries" style={styles.query}>
+      <ul>
+        {queries.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  </nav>
+);
+
+const Nav = connect(mapStateToProps)(ConnectedNav);
+export default Nav;
