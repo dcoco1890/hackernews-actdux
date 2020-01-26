eed62807bed8c22f412543286e1920f5f23d88da
Cohen <dcoco91@gmail.com>
2020-01-26 12:47:11 -0500

    changed http to https in api

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export default {
   getHomePage: function() {
-    return axios.get(`http://hn.algolia.com/api/v1/search?tags=front_page`);
+    return axios.get(`https://hn.algolia.com/api/v1/search?tags=front_page`);
   },
   getRecentPosts: function() {
     return axios.get(`https://hn.algolia.com/api/v1/search_by_date?tags=story`);
   },
   searchQueryTerm: function(query) {
     return axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}&tags=story`
+      `https://hn.algolia.com/api/v1/search?query=${query}&tags=story`
     );
   }
 };
