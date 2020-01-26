1a69526ab7c724b93ab96e47edde00fbeab50a12
Cohen <dcoco91@gmail.com>
2020-01-26 12:43:29 -0500

    Updates

---
+++ precache-manifest.6f6919fa4b1a347bc547ad8cbd226225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25deb62945d21f058fdd7fdfa7544376",
+    "url": "/hackernews-actdux/index.html"
+  },
+  {
+    "revision": "3085c920b963091ebbaf",
+    "url": "/hackernews-actdux/static/css/main.6bd7c6af.chunk.css"
+  },
+  {
+    "revision": "993bd369d9e8fcbb81c7",
+    "url": "/hackernews-actdux/static/js/2.29c948da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/hackernews-actdux/static/js/2.29c948da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3085c920b963091ebbaf",
+    "url": "/hackernews-actdux/static/js/main.bebfc95b.chunk.js"
+  },
+  {
+    "revision": "c4213cffed0694843aa3",
+    "url": "/hackernews-actdux/static/js/runtime-main.5d36b02f.js"
+  }
+]);
